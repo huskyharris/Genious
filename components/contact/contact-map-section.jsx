f366bd6c5f448650ed44412ece086b76cbd70a6c
huskyharris <esosaosaigbovo@gmail.com>
2024-09-08 08:55:30 -0500

    .

--- components/contact/contact-map-section.jsx
+++ components/contact/contact-map-section.jsx
@@ -21,10 +21,7 @@ const ContactMapSection = () => {
                 <iframe
                   width="100%"
                   height="600"
-                  frameborder="0"
                   scrolling="no"
-                  marginheight="0"
-                  marginwidth="0"
                   src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=bozener%20str.33%20moenchengladbach+(My%20Business%20Name)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
                 >
                   <a href="https://www.gps.ie/">gps vehicle tracker</a>
